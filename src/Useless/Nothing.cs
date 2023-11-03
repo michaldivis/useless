4d20bc8b05f9512c6f6b576e9d79447eb7ecd9c7
Michal Diviš <michall.divis@gmail.com>
2023-11-03 22:21:09 +0100

    feat: add XML comments

--- src/Useless/Nothing.cs
+++ src/Useless/Nothing.cs
@@ -1,7 +1,13 @@
 ﻿namespace Useless;
 
+/// <summary>
+/// Nothing at all.
+/// </summary>
 public static class Nothing
 {
+    /// <summary>
+    /// Do nothing...
+    /// </summary>
     public static void Do()
     {
     }
